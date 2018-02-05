3cbaf680d192adfe8fcf74483bee3036c1cdf544
moriya <tmj4oc3s_q26@i.softbank.jp>
2018-02-05 17:42:44 +0900

    AdmiDAO 完成　AdminDeleteAction AdminInsertItemAction 未完成

---
+++ src/com/internousdev/knit/action/AdminInsertItemAction.java
@@ -0,0 +1,30 @@
+package com.internousdev.knit.action;
+import java.util.Map;
+import org.apache.struts2.interceptor.SessionAware;
+import com.internousdev.knit.dao.AdmiDAO;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class AdminInsertItemAction extends ActionSupport implements SessionAware {
+	private String productId;
+	private String productName;
+	private String productNameKana;
+	private String productDescription;
+	private String categoryId;
+	private String imageFilePath;
+	private String imageFileName;
+	private String releaseCompany;
+	public Map<String,Object> sessuin;
+	private String errorMessage;
+	
+	public String execute(){
+		String result=SUCCESS;
+		if()
+		
+	}
+	
+	
+	
+	
+
+
+}
