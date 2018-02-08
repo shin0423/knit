1d1c17de3433be54cb9113cf88afa9ef1abbf5a9
nanri <nyandy1794@gmail.com>
2018-02-08 10:27:12 +0900

    レビュー機能DAO Action DTO 完成させました　南里

--- src/com/internousdev/knit/action/BuyItemInfoAction.java
+++ src/com/internousdev/knit/action/BuyItemInfoAction.java
@@ -1,20 +1,29 @@
 package com.internousdev.knit.action;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.internousdev.knit.dao.BuyItemInfoDAO;
+import com.internousdev.knit.dao.ReviewDAO;
 import com.internousdev.knit.dto.BuyItemDTO;
+import com.internousdev.knit.dto.ReviewDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class BuyItemInfoAction extends ActionSupport {
 	private String itemId;
 	private BuyItemDTO buyItemDTO = new BuyItemDTO();
+	private List<ReviewDTO> reviewList = new ArrayList<ReviewDTO>();
 
 
 	public String execute() throws SQLException{
 		buyItemDTO=null;
 		BuyItemInfoDAO buyItemInfoDAO = new BuyItemInfoDAO();
 		buyItemDTO=buyItemInfoDAO.selectBuyItemInfo(itemId);
+		ReviewDAO reviewDAO = new ReviewDAO();
+		setReviewList(reviewDAO.selectReviewAll(itemId));
+
+
 
 		return SUCCESS;
 
@@ -39,6 +48,16 @@ public class BuyItemInfoAction extends ActionSupport {
 
 	public void setItemId(String itemId) {
 		this.itemId = itemId;
+	}
+
+
+	public List<ReviewDTO> getReviewList() {
+		return reviewList;
+	}
+
+
+	public void setReviewList(List<ReviewDTO> reviewList) {
+		this.reviewList = reviewList;
 	}
 
 }
