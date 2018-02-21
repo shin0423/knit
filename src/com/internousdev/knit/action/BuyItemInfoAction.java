7c558d8118c1d1dafd0d35ff2a5d0993f7231804
nanri <nyandy1794@gmail.com>
2018-02-21 11:43:39 +0900

    a

--- src/com/internousdev/knit/action/BuyItemInfoAction.java
+++ src/com/internousdev/knit/action/BuyItemInfoAction.java
@@ -3,6 +3,10 @@ package com.internousdev.knit.action;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.struts2.interceptor.SessionAware;
 
 import com.internousdev.knit.dao.BuyItemInfoDAO;
 import com.internousdev.knit.dao.ReviewDAO;
@@ -10,16 +14,26 @@ import com.internousdev.knit.dto.BuyItemDTO;
 import com.internousdev.knit.dto.ReviewDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class BuyItemInfoAction extends ActionSupport {
+public class BuyItemInfoAction extends ActionSupport implements SessionAware{
 	private String itemId;
 	private int optionCount;
 	private BuyItemDTO buyItemDTO = new BuyItemDTO();
 	private List<Integer> optionNumber = new ArrayList<Integer>();
 	private List<ReviewDTO> reviewList = new ArrayList<ReviewDTO>();
 	private List<BuyItemDTO> categoryItemList = new ArrayList<>();
+	private String token;
+	public Map<String ,Object> session;
 
 
-	public String execute() throws SQLException{
+	@SuppressWarnings("static-access")
+	public String execute() throws SQLException {
+		//
+		RandomStringUtils rndStr = new RandomStringUtils();
+		token = rndStr.randomAlphabetic(10);
+		System.out.println("トークン値"+token);
+		setToken(token);
+		session.put("token", token);
+
 		buyItemDTO=null;
 		BuyItemInfoDAO buyItemInfoDAO = new BuyItemInfoDAO();
 		buyItemDTO=buyItemInfoDAO.selectBuyItemInfo(itemId);
@@ -94,6 +108,20 @@ public class BuyItemInfoAction extends ActionSupport {
 
 	public void setCategoryItemList(List<BuyItemDTO> categoryItemList) {
 		this.categoryItemList = categoryItemList;
+	}
+
+
+	public String getToken() {
+		return token;
+	}
+
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public void setSession(Map<String,Object> session){
+		this.session= session;
 	}
 
 }
