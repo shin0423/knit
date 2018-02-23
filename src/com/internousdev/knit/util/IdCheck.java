b1ae497815254c119869d8fd706375a8315aa1f5
koju <h04.kn-st.g00stf25@docomo.ne.jp>
2018-02-23 11:45:29 +0900

    すーぱーIDちぇっかー　南里

--- src/com/internousdev/knit/util/IdCheck.java
+++ src/com/internousdev/knit/util/IdCheck.java
@@ -1,5 +1,32 @@
-package com.internousdev.knit.util;
 
-public class IdCheck {
+	package com.internousdev.knit.util;
 
-}
+	import java.sql.SQLException;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.internousdev.knit.dao.CheckUserDAO;
+import com.opensymphony.xwork2.ActionSupport;
+
+	public class IdCheck extends ActionSupport implements SessionAware{
+		public Map<String,Object> session;
+//ゆーざーIDが存在する場合falseない場合はtrueをかえす
+		public boolean checkUser(String userId) throws SQLException{
+				CheckUserDAO dao = new CheckUserDAO();
+				boolean result = false;
+				if(!(dao.checkUserID(userId))){
+					result = true;
+				}
+				return result;
+			}
+
+
+
+
+		public void setSession(Map<String,Object> session){
+			this.session=session;
+		}
+	}
+
+
