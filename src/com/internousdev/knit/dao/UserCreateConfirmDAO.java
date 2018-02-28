bb26daa0595d94f5f41a6bba2d871111ce605f92
internousdev <internousdev@192.168.1.37>
2018-02-28 13:38:38 +0900

    syuusei

--- src/com/internousdev/knit/dao/UserCreateConfirmDAO.java
+++ src/com/internousdev/knit/dao/UserCreateConfirmDAO.java
@@ -3,22 +3,17 @@ package com.internousdev.knit.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.Map;
-
-import org.apache.struts2.interceptor.SessionAware;
 
 import com.internousdev.knit.dto.UserUpdateDTO;
 import com.internousdev.knit.util.DBConnector;
-import com.opensymphony.xwork2.ActionSupport;
 
-public class UserCreateConfirmDAO extends ActionSupport implements SessionAware{
+public class UserCreateConfirmDAO{
 	private DBConnector dbConnector = new DBConnector();
 	private Connection connection = dbConnector.getConnection();
-	public Map<String,Object> session;
 	UserUpdateDTO userUpdateDTO = new UserUpdateDTO();
 	public boolean getUserId(String userId){
 		//ユーザーIDの取得
-		String sql = "SELECT * FROM user_info where user_id=?";
+		String sql = "SELECT user_id FROM user_info where user_id=?";
         //ユーザーIDの確認
 		try{
 			PreparedStatement ps = connection.prepareStatement(sql);
@@ -35,12 +30,4 @@ public class UserCreateConfirmDAO extends ActionSupport implements SessionAware{
 		return false;
 	}
 
-	public Map<String,Object> getSession(){
-		return session;
-	}
-
-	@Override
-	public void setSession(Map<String,Object>session){
-		this.session=session;
-	}
 }
